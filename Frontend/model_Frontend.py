e90bfb9fd7c10cd86e604a892a17a6ee069d9120
douikB <100353244+douikB@users.noreply.github.com>
2025-07-01 17:53:42 +0900

    update .gitignore

--- Frontend/model_Frontend.py
+++ Frontend/model_Frontend.py
@@ -1,5 +1,7 @@
 import streamlit as st
-import torch
+from openai import OpenAI
+from dotenv import load_dotenv
+import torch, os
 from transformers import BertTokenizer, BertForSequenceClassification
 
 # 1. 모델과 토크나이저 불러오기
@@ -31,6 +33,53 @@ def classify_legal_issue(text):
         outputs = model(**inputs)
     pred = torch.argmax(outputs.logits, dim=1).item()
     return label_map[pred]
+
+################################################################################
+################################ Page Config ###################################
+################################################################################
+
+
+# Page Config 구성
+st.set_page_config(
+    page_title="Streamly - An Intelligent Streamlit Assistant",
+    page_icon="imgs/avatar_streamly.png",
+    layout="wide",
+    initial_sidebar_state="auto",
+    menu_items={
+        "Get Help": "https://github.com/khj200013/OpenA1/issues",
+        "Report a bug": "https://github.com/khj200013/OpenA1/issues",
+        "About": """
+            ## AI를 통해 개인정보 보호법에 위배되는 사항 확인
+            ### Powered using GPT-4
+
+            **GitHub**: https://github.com/khj200013/OpenA1
+            **문의 또는 버그 제보**: [Issue 등록하기](https://github.com/khj200013/OpenA1/issues)
+
+            이 AI 어시스턴트는 사용자의 상황이나 질문을 바탕으로  
+            **개인정보보호법 또는 관련 규정(GDPR 등)**에 따라  
+            위반 가능성을 분석하고, 관련 법령과 대응 방안을 안내합니다.
+        """
+    }
+)
+
+################################################################################
+################################ GPT UI 구성 ###################################
+################################################################################
+
+
+# GPT AI 구성
+load_dotenv()
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+client = OpenAI(api_key=OPENAI_API_KEY)
+
+
+if 'openai_model' not in st.session_state:
+    st.session_state.openai_model = 'gpt-4.1'
+
+if 'messages' not in st.session_state:
+    st.session_state.messages = []
+
+
 
 # 3. Streamlit UI 구성
 st.title("개인정보 법률 유형 분류 챗봇")
